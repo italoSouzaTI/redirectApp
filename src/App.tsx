41762d1042f0a7a295ba491078bb91243fb48762
Italo Araujo <italoasouzat.i@gmail.com>
2024-07-19 12:46:56 -0300

    fix: type any window

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import "./App.css";
 import { useEffect } from "react";
-function App() {
+type Window = {
+    FB: any;
+};
+function App({ FB }: Window) {
     useEffect(() => {
         const userAgent = navigator.userAgent || navigator.vendor;
         console.log(userAgent);
         // Detect iOS devices
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            window.location.href = "https://apps.apple.com/us/app/coimbra-motorista/id6450483010";
+        if (/iPad|iPhone|iPod/.test(userAgent) && !FB.MSStream) {
+            FB.location.href = "https://apps.apple.com/us/app/coimbra-motorista/id6450483010";
         }
         // Detect Android devices
         else if (/android/i.test(userAgent)) {
-            window.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appmotorista";
+            FB.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appmotorista";
         }
         // Redirect to a fallback page or show a message
         else {
-            window.location.href = "https://www.google.com";
+            FB.location.href = "https://www.google.com";
         }
     }, []);
     return (

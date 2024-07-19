6b9d0db23f1a749018f0a6ec9cff2c7ec16d2159
Italo Araujo <italoasouzat.i@gmail.com>
2024-07-19 12:56:35 -0300

    fix: type any window

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import "./App.css";
 import { useEffect } from "react";
-type Window = {
-    FB: any;
-};
+
+declare global {
+    interface Window {
+        MSStream?: any;
+    }
+}
 function App({ FB }: Window) {
     useEffect(() => {
         const userAgent = navigator.userAgent || navigator.vendor;
         console.log(userAgent);
-        // Detect iOS devices
-        if (/iPad|iPhone|iPod/.test(userAgent) && !FB.MSStream) {
-            FB.location.href = "https://apps.apple.com/us/app/coimbra-motorista/id6450483010";
+
+        // Detecta dispositivos iOS
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            window.location.href = "https://apps.apple.com/us/app/coimbra-motorista/id6450483010";
         }
-        // Detect Android devices
+        // Detecta dispositivos Android
         else if (/android/i.test(userAgent)) {
-            FB.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appmotorista";
+            window.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appmotorista";
         }
-        // Redirect to a fallback page or show a message
+        // Redireciona para uma página de fallback ou mostra uma mensagem
         else {
-            FB.location.href = "https://www.google.com";
+            window.location.href = "https://www.google.com";
         }
     }, []);
     return (
         <>
-            <p>redirect</p>
+            <p>Redirecionando para a loja de aplicativos apropriada...</p>
         </>
     );
 }

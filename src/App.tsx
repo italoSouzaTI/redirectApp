bee2084d32bb754632687cdce6df873bbd50faa3
italo Souza <italoasouzat.i@gmail.com>
2025-01-09 22:21:11 -0300

    feat: custom img

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import "./App.css";
 import { useEffect } from "react";
-
+import LogoGeral from "./assets/GERAL.svg";
+import LogoLogistica from "./assets/LOGISTICA.svg";
+import LogoTransporte from "./assets/TRANSPORTE.svg";
 declare global {
     interface Window {
         MSStream?: any;
     }
 }
 function App() {
-    useEffect(() => {
+    function handleImobilizado() {
         const userAgent = navigator.userAgent || navigator.vendor;
-        console.log(userAgent);
-
-        // Detecta dispositivos iOS
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            window.location.href = "https://apps.apple.com/us/app/coimbra-gest%C3%A3o-patrimonial/id6636491331";
+        }
+        // Detecta dispositivos Android
+        else if (/android/i.test(userAgent)) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appimobilizado";
+        }
+        // Redireciona para uma página de fallback ou mostra uma mensagem
+        else {
+            window.location.href = "https://www.google.com";
+        }
+    }
+    function handleGestaoMotorista() {
+        const userAgent = navigator.userAgent || navigator.vendor;
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            window.location.href = "https://apps.apple.com/us/app/coimbra-gest%C3%A3o-de-motorista/id6739811424";
+        }
+        // Detecta dispositivos Android
+        else if (/android/i.test(userAgent)) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.dcoimbra.appgestaomotorista";
+        }
+        // Redireciona para uma página de fallback ou mostra uma mensagem
+        else {
+            window.location.href = "https://www.google.com";
+        }
+    }
+    function handleMotorista() {
+        const userAgent = navigator.userAgent || navigator.vendor;
         if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             window.location.href = "https://apps.apple.com/us/app/coimbra-motorista/id6450483010";
         }
@@ -23,11 +50,22 @@ function App() {
         else {
             window.location.href = "https://www.google.com";
         }
-    }, []);
+    }
     return (
-        <>
-            <p>Redirecionando para a loja de aplicativos apropriada...</p>
-        </>
+        <div className="container">
+            <div className="card" onClick={handleImobilizado}>
+                <img src={LogoGeral} alt="Logo Imobilizado" />
+                <span className="textCuston">Imobilizado</span>
+            </div>
+            <div className="card" onClick={handleGestaoMotorista}>
+                <img src={LogoLogistica} alt="Logo Logística" />
+                <span className="textCuston">Supervisor Motorista</span>
+            </div>
+            <div className="card" onClick={handleMotorista}>
+                <img src={LogoTransporte} alt="Logo Transporte" />
+                <span className="textCuston">Motorista</span>
+            </div>
+        </div>
     );
 }
 
